3d4fa1883925d4ae6115195307f7252bbbc8c7ff
Pierre Rudloff <contact@rudloff.pro>
2014-07-27 14:18:01 +0200

    Localization French locale

--- ReviewAndMerge.i18n.php
+++ ReviewAndMerge.i18n.php
@@ -14,7 +14,38 @@
  * */
 $messages = array();
 
-$messages[ 'en' ] = array(
-    'reviewandmerge' => "Review and Merge"
+$messages['en'] = array(
+    'reviewandmerge' => "Review and Merge",
+    'origversion' => "Original version",
+    'revversion' => 'Reviewed version',
+    'keepedit' => 'Keep this edit',
+    'colon' => "&#58;",
+    'toggleinlinediff' => 'Toggle inline diff',
+    'validchanges' => 'Validate changes',
+    'nochangesreview' => 'No changes to review',
+    'onlyeauthorcanmergereviews' =>
+        'Only the author of an article can merge reviews.',
+    'mergedreviewsfrom' => 'Merged reviews from',
+    'pleasecheckchanges' => 'Please check that changes have been applied correctly',
+    'notenabledfornamespace' => 'Review and Merge is not enabled for this namespace.'
+);
+
+$messages['fr'] = array(
+    'reviewandmerge' => "Review and Merge",
+    'origversion' => "Version originale",
+    'revversion' => 'Version revisée',
+    'keepedit' => 'Garder cette modification',
+    'colon' => "&nbsp;&#58;",
+    'toggleinlinediff' => 'Afficher/masquer le diff en ligne',
+    'validchanges' => 'Valider les modifications',
+    'nochangesreview' => 'Aucune modification à afficher',
+    'onlyeauthorcanmergereviews' =>
+        "Seul l'auteur d'un article peut fusionner les révisions.",
+    'mergedreviewsfrom' => 'Fusion des révisions depuis',
+    'pleasecheckchanges' =>
+        'Veuillez vérifier que les modifications '.
+        .'ont été appliquées correctement',
+    'notenabledfornamespace' =>
+        "Review and Merge n'est pas activé pour cet espace de nom."
 );
 
